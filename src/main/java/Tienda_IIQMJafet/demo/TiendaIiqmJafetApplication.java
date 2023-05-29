6027adcaf72a7cdad8cb79a36977eb12f3b7f52a
jafet <jafet@LAPTOP-59QEOMGS>
2023-05-28 20:04:32 -0600

    Cambio 2

---
+++ src/main/java/Tienda_IIQMJafet/demo/TiendaIiqmJafetApplication.java
@@ -0,0 +1,13 @@
+package Tienda_IIQMJafet.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaIiqmJafetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaIiqmJafetApplication.class, args);
+	}
+
+}
