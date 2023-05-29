16dc4b93dd6f716afb2797576650712aed8355e3
jafet <jafet@LAPTOP-59QEOMGS>
2023-05-28 19:19:56 -0600

    Creacion de Proyecto Inicial

---
+++ src/test/java/com/Tienda_IIQM/TiendaIiqmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tienda_IIQM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaIiqmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
