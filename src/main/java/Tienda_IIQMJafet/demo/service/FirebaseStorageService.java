bfd69fb297d9f19f0cd7f3859da244dff26a464d
jafet <jafet@LAPTOP-59QEOMGS>
2023-07-21 19:02:39 -0600

    Semana 6

---
+++ src/main/java/Tienda_IIQMJafet/demo/service/FirebaseStorageService.java
@@ -0,0 +1,24 @@
+
+package Tienda_IIQMJafet.demo.service;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "xxidProjectxx.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-b660c-firebase-adminsdk-n1g4u-28663704c0.json";
+}
